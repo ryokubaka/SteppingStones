3c033b0d85a3dcac48886fe976b720d16b3b4444
ryokubaka <ryokubaka@gmail.com>
2025-03-17 15:04:17 -0400

    add extension to allow for offline deploy

--- event_tracker/__init__.py
+++ event_tracker/__init__.py
@@ -2,5 +2,6 @@ import os.path
 import duckdb
 
 if os.path.exists("db.sqlite3"):
+    duckdb.sql("SET global extension_directory='/opt/steppingstones/duckdb_extensions'")
     duckdb.sql("INSTALL sqlite; LOAD sqlite; SET GLOBAL sqlite_all_varchar=true;")
     duckdb.sql("CALL sqlite_attach('db.sqlite3');")
